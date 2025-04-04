540e102fc860c9c553dec80f6dfa6f0a063d8a6d
GermanS04 <german.salasmar@gmail.com>
2025-04-04 09:24:03 -0600

    added endpoint to get repository of projects

--- api/src/routes/project.ts
+++ api/src/routes/project.ts
@@ -1,7 +1,7 @@
 // File: poject.js
 import express from "express";
 import dotenv from "dotenv";
-import prisma from "../db/prisma.js";
+import prisma from "../db/prisma";
 
 dotenv.config();
 
@@ -10,6 +10,50 @@ const router = express.Router();
 router.get("/", async (req, res) => {
   console.log("Projects base");
   res.json({ message: "Projects base" });
+});
+
+router.get("/repositories", async (req, res) => {
+  try {
+    const currentDate = new Date();
+    const availableProjects = await prisma.projects.findMany({
+      where: {
+        AND: [
+          { start_date: { gt: currentDate } },
+          {
+            Project_Positions: {
+              some: {
+                user_id: null,
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        Project_Positions: {
+          where: {
+            user_id: null,
+          },
+        },
+        Region: true,
+        Users: true,
+      },
+    });
+
+    const formattedProjects = availableProjects.map((project) => ({
+      id: project.project_id,
+      name: project.project_name,
+      start_date: project.start_date,
+      end_date: project.end_date,
+      vacants: project.Project_Positions.length,
+      region: project.Region.region_name,
+      capability: project.Users.name,
+    }));
+
+    res.json(formattedProjects);
+  } catch (error) {
+    console.error("Error fetching available projects:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 export default router;
