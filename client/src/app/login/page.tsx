017954971b8a59b264b89f0c81ca18daa4c97aa4
Ultimate553 <diego.torred@gmail.com>
2025-04-07 15:34:22 -0600

    ok

--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/navigation";
 import { signIn, useSession } from "next-auth/react";
