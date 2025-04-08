0dcfc48c540ca9406fb1be21fc04e20421a0596d
GermanS04 <german.salasmar@gmail.com>
2025-04-08 16:40:28 -0600

    Endpoint skills of user

--- api/src/routes/employee.ts
+++ api/src/routes/employee.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import prisma from "../db/prisma";
+import { getUserIdFromSession } from "../utils/session";
 
 dotenv.config();
-
-
 
 const router = express.Router();
 
@@ -18,14 +17,16 @@ router.get("/user", async (req, res) => {
     const userId = parseInt(req.headers["user-id"], 10);
 
     if (!userId) {
-      return res.status(400).json({ error: "User ID is required in the headers." });
+      return res
+        .status(400)
+        .json({ error: "User ID is required in the headers." });
     }
 
     const user = await prisma.users.findUnique({
       where: { user_id: userId },
       include: {
-        Region: true, 
-        Permits: true, 
+        Region: true,
+        Permits: true,
       },
     });
 
@@ -36,6 +37,30 @@ router.get("/user", async (req, res) => {
     res.status(200).json(user);
   } catch (error) {
     console.error("Error fetching user data:", error);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+router.get("/skills", async (req, res) => {
+  try {
+    const userId = await getUserIdFromSession(req.headers["session-key"]);
+
+    if (!userId) {
+      return res
+        .status(400)
+        .json({ error: "User ID is required in the headers." });
+    }
+
+    const skills = await prisma.user_Skills.findMany({
+      where: { user_id: userId },
+      include: {
+        Skills: true,
+      },
+    });
+
+    res.json(skills);
+  } catch (error) {
+    console.error("Error fetching user skills:", error);
     res.status(500).json({ error: "Internal server error." });
   }
 });
