1d4f43800edcdfc4ad5b9b97f1f42dcc4be1be3b
Emi <emidmor@gmail.com>
2025-05-28 15:31:52 -0600

    [TS][Playwright]: Playwright-QASE connection completed!

--- playwright/tests/endpoints.spec.ts
+++ playwright/tests/endpoints.spec.ts
@@ -1,42 +1,52 @@
-import { test, expect, Page } from '@playwright/test';
-import { login } from './functions.spec';
+import { test, expect, Page } from "@playwright/test";
+import { login } from "./functions";
+import { qase } from "playwright-qase-reporter";
 
-test('GET employee/user/:userId', async ({ request, page }) => {
+test("Login", async ({ page }) => {
+  qase.id(76);
 
-    const userId = await login(page, 'EMP');
+  await login(page, 'EMP');
 
-    const response = await request.get(`http://localhost:3003/employee/user/${userId}`);
-    expect(response.ok()).toBeTruthy();
-
-    const data = await response.json();
-
-    expect(data).toMatchObject({
-        user_id: userId,
-        position_name: expect.any(String),
-        level: expect.anything(),
-        Country: expect.objectContaining({
-            country_name: expect.any(String),
-            timezone: expect.any(String),
-        }),
-        Certificate_Users: expect.any(Array),
-        Employee_Position: expect.any(Array),
-        Permits: expect.objectContaining({
-            is_admin: expect.any(Boolean),
-            is_employee: expect.any(Boolean),
-
-            role_id: expect.any(Number),
-        }),
-
-    });
 
 });
 
-test('GET getProjectById/:projectId', async ({ request, page }) => {
-  await login(page, 'CL'); 
+test("GET employee/user/:userId", async ({ request, page }) => {
+  const userId = await login(page, "EMP");
 
-  const projectId = 1; 
+  const response = await request.get(
+    `http://localhost:3003/employee/user/${userId}`
+  );
+  expect(response.ok()).toBeTruthy();
 
-  const response = await request.get(`http://localhost:3003/project/getProjectById/${projectId}`);
+  const data = await response.json();
+
+  expect(data).toMatchObject({
+    user_id: userId,
+    position_name: expect.any(String),
+    level: expect.anything(),
+    Country: expect.objectContaining({
+      country_name: expect.any(String),
+      timezone: expect.any(String),
+    }),
+    Certificate_Users: expect.any(Array),
+    Employee_Position: expect.any(Array),
+    Permits: expect.objectContaining({
+      is_admin: expect.any(Boolean),
+      is_employee: expect.any(Boolean),
+
+      role_id: expect.any(Number),
+    }),
+  });
+});
+
+test("GET getProjectById/:projectId", async ({ request, page }) => {
+  await login(page, "CL");
+
+  const projectId = 1;
+
+  const response = await request.get(
+    `http://localhost:3003/project/getProjectById/${projectId}`
+  );
   expect(response.ok()).toBeTruthy();
 
   const data = await response.json();
@@ -45,7 +55,7 @@ test('GET getProjectById/:projectId', async ({ request, page }) => {
     id: expect.any(Number),
     name: expect.any(String),
     description: expect.any(String),
-    start_date: expect.any(String), 
+    start_date: expect.any(String),
     end_date: expect.any(String),
     vacants: expect.any(Number),
     details: {
@@ -63,7 +73,7 @@ test('GET getProjectById/:projectId', async ({ request, page }) => {
       project_id: expect.any(Number),
       position_name: expect.any(String),
       position_desc: expect.any(String),
-      user_id: expect.anything(), 
+      user_id: expect.anything(),
       Project_Position_Skills: expect.any(Array),
       Project_Position_Certificates: expect.any(Array),
       Postulations: expect.any(Array),
--- playwright/tests/informes-y-analitica.spec.ts
+++ playwright/tests/informes-y-analitica.spec.ts
@@ -1,19 +1,27 @@
-import { login } from "./functions.spec";
+import { qase } from "playwright-qase-reporter";
+import { login } from "./functions";
 import { test, expect, Page } from '@playwright/test';
 
-// test('Employee correct information', async ({ request, page }) => {
+test('Check employee profile', async ({ page }) => {
+    qase.id(72);
 
-//     await login(page, );
+    await login(page, 'CL');
 
-//     // Click en la nav
-//     await page.locator('[href="/dashboard/repo-projects"]').click();
+    // Click en la navbar
+    await page.locator('[href="/dashboard/repo-empleados"]').click();
 
-//     const targetRow = page.locator('tr', { hasText: 'Proyecto PathExplorer' });
-//     await targetRow.click();
+    page.getByText("Abigail Parker");
 
-//     // Entramos al projecto
-//     const asignButton = targetRow.getByRole('button', { name: "Asignar"});
-//     await asignButton.click();
+    const targetRow = page.locator('tr', { hasText: 'Abigail Parker' });
 
+    const expandButton = targetRow.locator('[class="btn btn-ghost btn-sm"]');
+    await expandButton.click();
 
-// });
+    const profileButton = page.locator('tr', { hasText: 'Correo:' }).locator('button', { hasText: 'Ir a Perfil' });
+    await profileButton.click();
+
+    // Botón para abrir sub menú de trayectoria
+    await expect(page.getByText("Habilidades")).toBeVisible();
+    await expect(page.getByText("Técnicas")).toBeVisible();
+    await expect(page.getByText("Blandas")).toBeVisible();
+});
