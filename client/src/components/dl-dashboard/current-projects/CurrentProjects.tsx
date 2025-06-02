12831ccff8c7a9c791472036acab60d6e0cf4ed0
GermanS04 <german.salasmar@gmail.com>
2025-06-02 13:35:46 -0600

    erase search bar

--- client/src/components/dl-dashboard/current-projects/CurrentProjects.tsx
+++ client/src/components/dl-dashboard/current-projects/CurrentProjects.tsx
@@ -82,60 +82,6 @@ const CurrentProjects = () => {
       <div className="w-full">
         <h2 className="text-3xl font-bold mb-4">Proyectos en Curso</h2>
         <div className="w-full h-px bg-base-300 mb-4"></div>
-        <div className="flex flex-row w-full h-14 px-8 mb-4">
-          <input
-            type="text"
-            placeholder="Buscar..."
-            className="input input-bordered h-full w-10/12 py-4.5 border-primary text-primary placeholder:text-primary text-base outline-none focus:outline-none"
-            style={{
-              backgroundColor: "oklch(from var(--color-accent) l c h / 30%)",
-            }}
-          />
-          <div className="relative inline-block w-2/12" ref={dropdownRef}>
-            <button
-              className="border border-primary text-start text-primary w-full ml-4 h-full pl-4 rounded-lg hover:cursor-pointer"
-              style={{
-                backgroundColor: "oklch(from var(--color-accent) l c h / 30%)",
-              }}
-              onClick={() => setShowCalendarInput(!showCalendarInput)}
-            >
-              Fechas
-            </button>
-            {showCalendarInput && (
-              <div className="absolute right-0 mt-2 z-20 w-64 p-4 bg-white rounded-lg shadow border">
-                <label className="block mb-2 text-sm font-medium text-gray-600">
-                  Desde
-                </label>
-                <div className="relative">
-                  <input
-                    type="date"
-                    className="input input-bordered w-full mb-4"
-                    value={startDate}
-                    onChange={(e) => setStartDate(e.target.value)}
-                  />
-                  <div className="absolute right-3 top-4/12 -translate-y-1/2 pointer-events-none">
-                    <FaCalendarAlt className="text-gray-400 text-xl" />
-                  </div>
-                </div>
-
-                <label className="block mb-2 text-sm font-medium text-gray-600">
-                  Hasta
-                </label>
-                <div className="relative">
-                  <input
-                    type="date"
-                    className="input input-bordered w-full"
-                    value={endDate}
-                    onChange={(e) => setEndDate(e.target.value)}
-                  />
-                  <div className="absolute right-3 top-6/12 -translate-y-1/2 pointer-events-none">
-                    <FaCalendarAlt className="text-gray-400 text-xl" />
-                  </div>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
           {firstSixProjects.map((project, index) => (
             <div
